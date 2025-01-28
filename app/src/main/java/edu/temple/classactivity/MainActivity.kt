d98f2fed583a565e1b979cfdf6802d6e4b18906c
Maab Taha <tmaab13@gmail.com>
2025-01-28 12:14:34 -0500

    Click me button wired to onclick handler

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,26 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var clickMeButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        clickMeButton = findViewById(R.id.button3);
+
+        clickMeButton.setOnClickListener{
+            clickMeButton.text = "Button clicked"
+        }
     }
+
 }
